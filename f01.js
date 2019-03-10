5900fd3e44f2998984a75ade29b2417ce85dc256
Bill local <Bill@local>
2019-03-10 16:22:43 -0300

    first commit f03.js

--- f01.js
+++ f01.js
@@ -7,3 +7,19 @@ const x = ()=>{
 } 
 
 x();
+
+/// nova function
+
+BigDick = {
+   Size: 28,
+   Color: "Black", 
+   Circle: (Math.PI * (14 / 2)),
+   CumLoad: "200ml"
+}
+
+
+function pussyDestroyer(){
+	console.log("This pussy has been destroyed by a: " + BigDick.Size + " long and " + Math.floor(BigDick.Circle) + " round dick ");
+}
+
+pussyDestroyer();
